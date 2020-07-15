eeb2b4128d0a96f35449fea762b93f755ea8f9d4
xuguo <xuguo@leimingtech.com>
2020-07-15 09:38:09 +0800

    build(rollup): 增加cjs和esm模块输出
    增加cjs和esm模块输出 添加 resolve和commonjs插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
 const path = require('path')
 
 const resolvePath = (p) => path.resolve(__dirname, './', p)
@@ -6,8 +8,18 @@ export default {
   input: resolvePath('src/index.js'),
   outupt: [
     {
-      name: 'fruitscript.js',
-      format: 'umd'
+      file: 'fruitscript.js',
+      format: 'umd',
+      name: 'fruitscript'
+    },
+    {
+      file: 'fruitscript.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'fruitscript.es.js',
+      format: 'esm'
     }
-  ]
+  ],
+  plugins: [resolve(), commonjs()]
 }
