24058084e3362f94ac87e551af6662e1614feb85
guo.xu <xuguo@outlook.it>
2021-03-21 15:28:19 +0800

    chore: init项目

---
+++ xus.config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from '@xus/cli'
+
+export default defineConfig({
+  libBuild: {
+    target: 'esnext',
+    formats: ['esm', 'cjs'],
+    minify: false,
+    sourcemap: false,
+    alwaysEmptyDistDir: true
+  },
+  lint: {
+    eslint: {
+      include: './src/**/*',
+      ext: ['.ts']
+    },
+    stylelint: false
+  },
+  release: {
+    beforeRelease: [
+      {
+        bin: 'npm',
+        args: ['run', 'test'],
+        message: {
+          start: 'Test start',
+          succeed: 'Test succeed',
+          failed: 'Test failed'
+        }
+      },
+      {
+        bin: 'npm',
+        args: ['run', 'lint-fix'],
+        message: {
+          start: 'Lint start',
+          succeed: 'Lint succeed',
+          failed: 'Lint failed'
+        }
+      },
+      {
+        bin: 'npm',
+        args: ['run', 'build'],
+        message: {
+          start: 'Build start',
+          succeed: 'Build succeed',
+          failed: 'Build failed'
+        }
+      }
+    ],
+    branch: 'main'
+  }
+})
