cabc0f645ed61994a86c6d373c4d9fdb2b499a05
guo.xu <xuguo@outlook.it>
2021-03-24 21:20:27 +0800

    wip: 流处理及token基础类

---
+++ src/shared/peekIt.ts
@@ -0,0 +1,69 @@
+import { LinkedList, CompareFn, Compare } from '@xus/data-structures'
+
+const compareString: CompareFn<string> = (l, r) => {
+  // linkedlist compare just need string Equal
+  if (l === r) return Compare.EQUAL
+  return Compare.SMALL
+}
+
+const CACHE_SIZE = 10
+
+/**
+ * 4 --> 3 --> 2 --> 1
+ * time cache move to right
+ * stream move to left
+ */
+export class PeekIt {
+  private timeCache
+  private putbackStack
+  constructor(private it: Iterator<string, void>, private end: string = '') {
+    this.timeCache = new LinkedList<string>(compareString)
+    this.putbackStack = new LinkedList<string>(compareString)
+  }
+
+  hasNext() {
+    return !!this.end || !!this.peek()
+  }
+
+  next() {
+    let val: string
+    if (this.putbackStack.size() > 0) {
+      val = this.putbackStack.deleteIndex(0)!
+    } else {
+      const next = this.it.next()
+      if (next.done) {
+        const endToken = this.end
+        this.end = ''
+        val = endToken
+      } else {
+        val = next.value
+      }
+    }
+    while (this.timeCache.size() > CACHE_SIZE - 1) {
+      this.timeCache.deleteIndex(0)
+    }
+    this.timeCache.addLast(val)
+    return val
+  }
+
+  peek() {
+    // optional
+    if (this.putbackStack.size() > 0) {
+      const val = this.putbackStack.deleteIndex(0)!
+      this.putbackStack.addFirst(val)
+      return val
+    }
+
+    const val = this.next()
+    this.putBack()
+    return val
+  }
+
+  putBack() {
+    if (this.timeCache.size() > 0) {
+      this.putbackStack.addFirst(
+        this.timeCache.deleteIndex(this.timeCache.size() - 1)!
+      )
+    }
+  }
+}
---
+++ src/shared/token.ts
@@ -0,0 +1,43 @@
+export enum TokenType {
+  KEYWORD = 'KEYWORD',
+  VARIABLE = 'VARIABLE',
+  OPERATOR = 'OPERATOR',
+  BRACKET = 'BRACKET',
+  NUMBER = 'NUMBER',
+  BOOLEAN = 'BOOLEAN',
+  STRING = 'STRING'
+}
+
+const TypeKeyword = ['num', 'bool', 'string', 'void']
+
+export class Token {
+  constructor(private _type: TokenType, private _value: string) {}
+
+  get type(): TokenType {
+    return this._type
+  }
+
+  get value(): string {
+    return this._value
+  }
+
+  isVariable() {
+    return this._type === TokenType.VARIABLE
+  }
+
+  isScaler() {
+    return (
+      this._type === TokenType.NUMBER ||
+      this._type === TokenType.STRING ||
+      this._type === TokenType.BOOLEAN
+    )
+  }
+
+  isType() {
+    return TypeKeyword.includes(this._value)
+  }
+
+  toString() {
+    return `type: ${this._type}, value: ${this._value}`
+  }
+}
